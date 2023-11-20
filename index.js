1128e502672fbbc2bb44e0e6b6099f9e25f7a3b3
gajavegs <gauravsg2004@gmail.com>
2023-11-19 19:50:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var helloWorld = require('./helloWorld.js'); // HelloWorld router (try refreshing)
 app.use('/test',helloWorld);
 
-var auth = require('./auth.js'); // HelloWorld router (try refreshing)
+var auth = require('./auth.js'); // Auth router (try CRUD-ing)
 app.use('/auth',auth);
 
 app.listen(3000);
