82a423e4f907ec8d6016f298877c14a795246569
gajavegs <gauravsg2004@gmail.com>
2023-11-27 00:01:15 -0500

    Fixed json repetitions

--- courses.js
+++ courses.js
@@ -84,22 +84,12 @@ async function publicSiteUp() {
     return content;
 }
 
-function yearsSince1970(){
-    const minute = 1000 * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-    const year = day * 365;
-    
-    return Math.round(Date.now() / year);
-}
-
 async function getCourses(year) {
     // puppeteering
     const browser = await puppeteer.launch({headless: "new"});
     const page = await browser.newPage();
     // Public course listing site
-    const response = await page.goto(publicSite(year), { timeout: 30000 } ); // for scraping add options like network2 or whatever; we can vary getting the source html (like in this case) or getting what the user actually sees after some js shenanigans with these options
-    let content = await response.text();
+    await page.goto(publicSite(year), { timeout: 30000 } ); // for scraping add options like network2 or whatever; we can vary getting the source html (like in this case) or getting what the user actually sees after some js shenanigans with these options
 
     await page.screenshot({path: 'files/screenshot5.png'});
     
@@ -117,6 +107,28 @@ async function getCourses(year) {
     await getPageCourses(page,toRet);
 
     return toRet;
+}
+
+async function getSections(username,password) {
+    // puppeteering
+    const browser = await puppeteer.launch({headless: "new"});
+    const page = await browser.newPage();
+    // Banner web schedule site
+    await page.goto(bannerSite, { timeout: 30000 } );
+
+    // Inputting username/password
+    await page.click('input[id="usernameUserInput"]');
+    await page.keyboard.type(username, {delay: 100});
+    await page.click('input[id="password"]');
+    await page.keyboard.type(password, {delay: 100});
+
+    await page.keyboard.press('Enter');
+    
+    // Waiting for schedule page to load
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    await page.screenshot({path: 'files/screenshot7.png'});
+
 }
 
 // Length changed because no longer at limit of course length
@@ -140,7 +152,9 @@ async function getPageCourses(page, toRet) {
         // console.log("id and name: "+id_and_name);
         // console.log("dept and number: "+dept_and_number);
         // await browser.close();
-        toRet.push(course_factory(dept_and_number[1],dept_and_number[0],id_and_name[1])); // id, dept, name
+        if (dept_and_number[1] && dept_and_number[0] && id_and_name[1]) { // prvents incomplete course data
+            toRet.push(course_factory(dept_and_number[1],dept_and_number[0],id_and_name[1])); // id, dept, name
+        }
     }
 }
 
@@ -183,6 +197,10 @@ function curMonth(){
     return DateTime.now().month;
 }
 
+function thisYear(){
+    return DateTime.now().year;
+}
+
 // Overview
   // This API will allow for the maintenance and use of a publicly available database of Rose-Hulman's course offerings
 
@@ -195,6 +213,7 @@ function curMonth(){
         // Get - The public site is up/we logged in right (or at least has the html we expect)
     // Data acquisition (IN PROGRESS)
         // NOTE: We can make this more flexible and parametrize by year, professor, etc. to update information in only parts of the db but waiting for mongodb first since I don't want to implement allat in json
+        // NOTE: New folders for each new year represented, with each one containing the respective courses and sections jsons
         // Put - Write all courses from public site into 20XX_courseinfo.json. Year specified is the later of xxxx-yyyy, aka the year the class of yyyy graduates
             // courseinfo needs to be organized by department, then course id/name
         // Put - Write all sections from banner site into 20XX_sectioninfo.json (depends on corresponding courseinfo.json). // Write all courses from public site into 20XX_courseinfo.json. Year specified is the later of xxxx-yyyy, aka the year the class of yyyy graduates
@@ -253,22 +272,22 @@ router.put('/update_archive/public',async function(req,res) {
 });
 // Write all courses from public site into 20XX_courseinfo.json. Year specified is the later of xxxx-yyyy, aka the year the class of yyyy graduates
 router.put('/load_courses/:year',async function(req,res) {
-    // prolly gonna be async
-    // return an error for stupid offsets. I'll also create new folders for each new year represented, with each one containing the respective courses and sections jsons
-    let curYear = yearsSince1970()+1970;
+    let curYear = thisYear();
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     // Can't tell the future
     if (req.params.year > curYear+1) {
         res.send("Invalid year: "+req.params.year);
     }
-    // Since summer registrations are over and the public site will be aimed at potential new students, it'll switch over in May to the next school year. Before May it will not have switched and therefore the next year will not yet be valid.
-    if ((req.params.year == curYear+1 && curMonth() < 4)) {
+    // Since summer registrations are over and the public site will be aimed at potential new students, it'll switch over in May to the next school year.
+    // Before April it will probably not have switched and therefore the next year will not yet be valid.
+    if ((req.params.year == curYear+1 && curMonth() < 3)) {
         res.send("Invalid month. Too early in the year for next years schedule: "+months.get(curMonth())+"\n(Correct if I'm wrong)");
     }
 
     // So we assume the site will switch over in May, so anything after that "current" will be next year, and we assume "prev-year" will start existing
     let year = "";
     // We know there will be a valid url for the given year
+    console.log("Croy ear"+(curYear+1));
     if (req.params.year == curYear+1 || (req.params.year == curYear && curMonth() < 4)) { // Means next year and we know it's valid, so we look at the latest ("current"), or we want this year and it hasn't switched yet
         year = "current";
     } else { // So we're either looking at this year or years previous once they've been superceded by a current
@@ -278,11 +297,17 @@ router.put('/load_courses/:year',async function(req,res) {
     // stepping through all 39 pages, will likely involve another puppeteer function to await
     let courses = await getCourses(year);
     await writeCourses(courses,req.params.year);
-    // I should look into some html to json solutions
     res.end();
 });
 // Write all sections from banner site into 20XX_sectioninfo.json (depends on corresponding courseinfo.json). // Write all courses from public site into 20XX_courseinfo.json. Year specified is the later of xxxx-yyyy, aka the year the class of yyyy graduates
-router.put('/load_sections/:year',async function(req,res) {
-
+router.put('/load_sections/:year/:username/:password',async function(req,res) {
+    let curYear = thisYear();
+    // Can't tell the future
+    if (req.params.year > curYear+1) {
+        res.send("Invalid year: "+req.params.year);
+    }
+    let sections = await getSections(req.params.username, req.params.password);
+    await writeSections(sections,req.params.year);
+    res.end();
 });
 module.exports = router;
